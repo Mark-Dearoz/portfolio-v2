bf28a06937aa4e600f95e5166c079433d442311e
Mark Dearoz <markdearoz08@gmail.com>
2021-09-12 21:53:07 -0400

    text edit

--- src/Sections/About.js
+++ src/Sections/About.js
@@ -11,10 +11,10 @@ const About = () => {
 			</motion.h1>
 			<motion.p className={style.p} ref={pRef} style={{ opacity }}>
 				<span className={style.name}>MARK DEAROZ</span> is a software engineer based in New York,
-				NY. He recieved his bachelors on Electrical and Computer Engineer and persued his masters on
+				NY. He received his bachelors on Electrical and Computer Engineer and pursued his masters on
 				Computer Science. His passion is to create innovative and functional applications without a
 				compromise on design and esthetic. Mark has knowledge on the bleeding edge technologies of
-				front-end development. He also loves to learn all about the emerging technolgies in tech,
+				front-end development. He also loves to learn all about the emerging technologies in tech,
 				and improve his skills as a software engineer.
 			</motion.p>
 		</div>
